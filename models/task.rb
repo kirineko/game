a04157a7f79ca4e98dc657e71bb2d22ac1777a73
kirineko <kirinekoneko@gmail.com>
2018-07-28 18:25:30 +0800

    更新了index.erb

--- models/task.rb
+++ models/task.rb
@@ -2,7 +2,7 @@ ActiveRecord::Base.establish_connection(
   adapter:  "mysql2",
   host:     "localhost",
   username: "root",
-  password: "root",
+  password: "",
   database: "sample_app_production"
 )
 class Task < ActiveRecord::Base 
